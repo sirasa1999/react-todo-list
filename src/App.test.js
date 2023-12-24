c3b83e41eaad376c8d8fb6e5673a49ed2ed68663
Sirasa <sirasatuladhar.com>
2023-12-24 17:23:15 +0545

    Todo app from react

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper />
+    </div>
+  );
+}
+
+export default App;
